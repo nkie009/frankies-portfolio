3a7d029fc021b17254853c23c53dea72f8e1071f
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-06 16:51:02 +1100

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // "./src/components/About.js",
+    // "./src/components/Contact.js",
+    // "./src/components/Navbar.js",
+    // "./src/components/Projects.js",
+    // "./src/components/Skills.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
