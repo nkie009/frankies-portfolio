51791d1008400d37825e53f3b15b3e829d97a7db
Frankie T <frankie.tsoi@hotmail.com>
2022-03-29 16:15:53 +1100

    fixing css in projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,4 @@
 import React from 'react'
-
-
 import './Projects.scss'
 
 export default function Projects() {
@@ -9,24 +7,23 @@ export default function Projects() {
 
   return (
     <div className="projects" id="myProjects">
-    <h1>myProjects</h1>
       <div className="slider">
-
         <div className="container">
           <div className="works">
-
-            <div className="leftPro"></div>
+            <div className="leftPro">
+              <div className="leftContainer">
+                {/* <div className="imgContainer">
+                  <img src="assets/Rhino.png" alt="" />
+                </div> */}
+                <h2>a-z learning</h2>
+                <p>A - Z Learning is an educational website with target audiences at the age of 2 to 5 years old children. Features: a -z dictionary with a button that can randomly pick an item that starts with the alphabet letter. Mini-game for children to play with.</p>
+              <span>live link</span>
+              </div>
+            </div>
             <div className="rightPro"></div>
-
           </div>
-
         </div>
-
-
-
       </div>
-
-
     </div>
   )
 }
